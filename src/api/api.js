a6552d75c0a11c3ebe338e0a7484450c59221086
anngreen <anngreen@qq.com>
2023-01-13 21:53:30 +0800

    base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 import request from "@/utils/request";
 
+let BASE_URL = 'https://api.uomg.com'
 // 随机土味情话
 export const randqinghua = data => {
     data = data || {};
     return request({
-        url: `https://api.uomg.com/api/rand.qinghua`,
+        url: `${BASE_URL}/api/rand.qinghua`,
         method: "post",
         data
     })
