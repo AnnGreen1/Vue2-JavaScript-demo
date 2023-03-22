d8e98e24744b9e982309250a31050c499421801a
anngreen <anngreen@qq.com>
2023-03-22 21:44:17 +0800

    echarts demo

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,22 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/JavascriptAxios.vue')
   },
+  {
+    path: '/echarts',
+    name: 'echarts',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EchartsDemo.vue')
+  },
+  {
+    path: '/echartsii',
+    name: 'echartsii',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/EchartsDemoII.vue')
+  },
   ...commonroute
 ]
 
