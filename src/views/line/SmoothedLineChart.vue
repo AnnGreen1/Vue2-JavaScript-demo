49d073dcd86a45cf7ba79d8730a79d25d880fa1e
anngreen <anngreen@qq.com>
2023-07-18 10:30:53 +0800

    平滑曲线图

--- src/views/line/SmoothedLineChart.vue
+++ src/views/line/SmoothedLineChart.vue
@@ -25,12 +25,12 @@ export default {
         yAxis: {
           type: "value",
           inverse: true,
-          min:1400,
-          max:800
+          min: 100,
+          max: 0,
         },
         series: [
           {
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            data: [1, 45, 6, 76, 23, 56, 34, 45],
             type: "line",
             smooth: true,
           },
