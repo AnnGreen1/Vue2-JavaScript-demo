8335903ed9c0b1e0881d6b226b823bb517b5d6ab
anngreen <anngreen@qq.com>
2023-08-27 22:56:34 +0800

    tips

--- src/views/Dev/MockjsDemo.vue
+++ src/views/Dev/MockjsDemo.vue
@@ -4,7 +4,7 @@
 
 <script>
 /**
- * @description: mockjs会拦截所有的请求，对于mockjs中定义了的请求就会直接拦截返回mock结果，而对于没有定义的mock再次发起真实请求
+ * @description: mockjs会拦截所有的请求，对于mockjs中定义了的请求就会直接拦截返回mock结果，而对于没有定义的mock再次发起真实请求，并且使用postman等工具是不能直接向mock接口发起请求的
  */
 import { mockjsDemo } from "@/api/mockjs";
 export default {
