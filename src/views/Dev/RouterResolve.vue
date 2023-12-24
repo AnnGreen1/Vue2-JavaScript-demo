8659a353b9889626366b2e6b972ede79d625e764
anngreen <anngreen@qq.com>
2023-12-24 14:12:17 +0800

    使用href在新标签页打开一个vue路由的另一种方法

--- src/views/Dev/RouterResolve.vue
+++ src/views/Dev/RouterResolve.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        <button @click="openRoute">直接open路由</button>
-    </div>
+  <div>
+    <button @click="openRoute">直接open路由</button>
+    <button @click="openThenHref">
+      能否使用window.open和location.href配合
+    </button>
+  </div>
 </template>
 
 <script>
@@ -9,23 +12,24 @@
  * 需求：如何在新标签页打开一个 vue router 路由地址？
  */
 export default {
-    data() {
-        return {
-
-        }
-    },
-    methods: {
-        openRoute() {
-            const routeUrl = this.$router.resolve({
-                name: 'about',
-                params: {
-                    talkId: 'fasdfa7sdf79as7d8f9a0sd980'
-                },
-            });
-            console.log(routeUrl);
-            window.open(routeUrl.href, '_blank');
+  data() {
+    return {};
+  },
+  methods: {
+    openRoute() {
+      const routeUrl = this.$router.resolve({
+        name: "about",
+        params: {
+          talkId: "fasdfa7sdf79as7d8f9a0sd980",
         },
+      });
+      console.log(routeUrl);
+      window.open(routeUrl.href, "_blank");
     },
-
-}
+    openThenHref() {
+      var newWindow = window.open("about:blank"); // 打开一个新的空白标签页
+      newWindow.location.href = "/JavascriptAxios"; // 在新标签页中跳转到某个路由
+    },
+  },
+};
 </script>
